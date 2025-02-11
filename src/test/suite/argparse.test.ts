898b2264769332f27574ae39cce60381af378232
Greg Savin <gsavin@mips.com>
2025-02-11 15:31:35 -0800

    Fix arg parsing tests

--- src/test/suite/argparse.test.ts
+++ src/test/suite/argparse.test.ts
@@ -5,30 +5,30 @@ import * as argparse from '../../argparse';
 suite('Argument Parser Test Suite', () => {
     vscode.window.showInformationMessage('Start argument parser tests.');
 
-    const prefix = 'Argument parser should be able to handle argument strings ';
+    const prefix = 'Argument parser should be able to ';
 
-    test(prefix + 'that do not use quoting', () => {
+    test(prefix + 'handle argument strings that do not use quoting', () => {
         const args = argparse.parseArgs('-vmargs -Dsome.property=value_without_spaces');
         assert.deepEqual(args, ['-vmargs', '-Dsome.property=value_without_spaces']);
     });
 
-    test(prefix + 'that have extra whitespace between arguments', () => {
+    test(prefix + 'handle argument strings that have extra whitespace between arguments', () => {
         const args = argparse.parseArgs('   -vmargs    -Dsome.property=value_without_spaces \t  ');
         assert.deepEqual(args, ['-vmargs', '-Dsome.property=value_without_spaces']);
     });
 
-    test(prefix + 'that use quoting', () => {
+    test(prefix + 'handle argument strings that use quoting', () => {
         const args = argparse.parseArgs('   -vmargs    -Dsome.property="value with spaces" \t  ');
         assert.deepEqual(args, ['-vmargs', '-Dsome.property=value with spaces']);
-    });
-
-    test(prefix + 'that use quotes with embedded literal quotes (using a sequence of 2 consecutive quotes)', () => {
-        const args = argparse.parseArgs('   -vmargs    -Dsome.property="""in literal quotes""" \t  ');
-        assert.deepEqual(args, ['-vmargs', '-Dsome.property="in literal quotes"']);
     });
 
     test(prefix + 'recognize non-nested consecutive quotes as an empty string', () => {
         const parsed = argparse.parseArgs('empty_value=""');
         assert.deepEqual(parsed, ['empty_value=']);
+    });
+
+    test(prefix + 'recognize nested consecutive quotes as a literal quote character', () => {
+        const args = argparse.parseArgs('   -vmargs    -Dsome.property="""in literal quotes""" \t  ');
+        assert.deepEqual(args, ['-vmargs', '-Dsome.property="in literal quotes"']);
     });
 });
